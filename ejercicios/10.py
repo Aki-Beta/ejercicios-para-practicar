2f3bba91367d4bf8e93f4c330a5acade7c719847
Aki-Beta <angelagarciatorres1998@gmail.com>
2026-03-30 14:59:41 -0500

    Update 10.py

--- ejercicios/10.py
+++ ejercicios/10.py
@@ -19,3 +19,31 @@ def clasificar_asistencia(clases):
 
 clases = int(input("¿Cuántas clases asistió en el mes? "))
 print(clasificar_asistencia(clases))
+
+def menu():
+    while True:
+        print("\n1. Add student")
+        print("2. Show students")
+        print("3. Find student")
+        print("4. Update student")
+        print("5. Delete student")
+        print("6. Exit")
+
+        option = input("Choose an option: ")
+
+        if option == "1":
+            add_student()
+        elif option == "2":
+            show_students()
+        elif option == "3":
+            find_student()
+        elif option == "4":
+            update_student()
+        elif option == "5":
+            delete_student()
+        elif option == "6":
+            break
+        else:
+            print("Invalid option")
+
+menu()
